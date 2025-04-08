5fd56f9c51a236bec3a2ffe2b13c09b34f1b99f5
kosalaprasetya <kosalaprasetya@gmail.com>
2025-04-08 14:44:53 +0700

    feat: set up Express server with CORS and JSON parsing

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
