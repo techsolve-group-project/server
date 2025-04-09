e4f3389a9c30df380f68bcca8dd22b152bae2e78
hafidzahm <hafizhahm123@gmail.com>
2025-04-09 18:10:59 +0700

    chore: Add socket.io dependency and implement socket server in app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const { router } = require("./routes/root.route");
 const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +18,13 @@ app.use(router);
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
+io.on("connection", (socket) => {
+  // ...
+  console.log(socket);
+  console.log(socket.id, '<------ connected socket id');
+  socket.emit('welcome_msg', 'Welcome to TechSolve Server!')
+});
+
+httpServer.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
